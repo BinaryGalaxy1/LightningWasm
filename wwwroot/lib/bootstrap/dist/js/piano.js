9f6ddf0c06b491007cec60df14b32b877d3e2438
BinaryGalaxy <25@u2id.com>
2025-05-29 06:34:15 -0400

    lots of keys

--- wwwroot/lib/bootstrap/dist/js/piano.js
+++ wwwroot/lib/bootstrap/dist/js/piano.js
@@ -13,30 +13,33 @@ window.playPianoNote = function(note) {
 };
 // Map note names to frequencies (C4 = 261.63 Hz)
 function getFrequency(note) {
-    const notes = {
-        'C': 261.63,
-        'C#': 277.18,
-        'D': 293.66,
-        'D#': 311.13,
-        'E': 329.63,
-        'F': 349.23,
-        'F#': 369.99,
-        'G': 392.00,
-        'G#': 415.30,
-        'A': 440.00,
-        'A#': 466.16,
-        'B': 493.88,
-        'C2': 523.25
+    // note format: e.g. A2, C#4, F5
+    const noteRegex = /^([A-G]#?)(\d)$/;
+    const semitones = {
+        'C': 0, 'C#': 1, 'D': 2, 'D#': 3, 'E': 4, 'F': 5, 'F#': 6, 'G': 7, 'G#': 8, 'A': 9, 'A#': 10, 'B': 11
     };
-    return notes[note] || 261.63;
+    const match = note.match(noteRegex);
+    if (!match) return 261.63;
+    const [_, n, o] = match;
+    const octave = parseInt(o, 10);
+    // A4 = 440Hz, MIDI 69
+    const midi = (octave + 1) * 12 + semitones[n];
+    return 440 * Math.pow(2, (midi - 69) / 12);
 }
 
 
 // Keyboard mapping for the piano (avoid redeclaration)
 if (!window.keyToNote) {
     window.keyToNote = {
-        'a': 'C', 'w': 'C#', 's': 'D', 'e': 'D#', 'd': 'E', 'f': 'F', 't': 'F#',
-        'g': 'G', 'y': 'G#', 'h': 'A', 'u': 'A#', 'j': 'B', 'k': 'C2'
+        // QWERTY row (white keys)
+        'q': 'A2', 'w': 'B2', 'e': 'C3', 'r': 'D3', 't': 'E3', 'y': 'F3', 'u': 'G3',
+        'i': 'A3', 'o': 'B3', 'p': 'C4', '[': 'D4', ']': 'E4', '\\': 'F4',
+        // Number row (black keys)
+        '2': 'A#2', '3': 'C#3', '4': 'D#3', '6': 'F#3', '7': 'G#3', '8': 'A#3', '0': 'C#4', '-': 'D#4',
+        // zxcvbnm, ,./' (white keys)
+        'z': 'G4', 'x': 'A4', 'c': 'B4', 'v': 'C5', 'b': 'D5', 'n': 'E5', 'm': 'F5', ',': 'G5', '.': 'A5', '/': 'B5', "'": 'C6',
+        // asdfghjk (black keys)
+        'a': 'G#4', 's': 'A#4', 'd': 'C#5', 'f': 'D#5', 'g': 'F#5', 'h': 'G#5', 'j': 'A#5', 'k': 'C#6'
     };
 }
 
